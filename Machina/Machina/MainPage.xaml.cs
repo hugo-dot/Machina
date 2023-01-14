d0139f06466cbd55337cf6a25735a008521a21a7
hugo <79638201+hugo-dot@users.noreply.github.com>
2023-01-14 13:11:43 +0100

    init commit

---
+++ Machina/Machina/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Xamarin.Forms;
+
+
+namespace Machina
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void StartButtonClicked(object sender, EventArgs e)
+        {
+            StartButtonClickedAsync();
+           
+        }
+        private async Task StartButtonClickedAsync()
+        {
+            
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+            //await DisplayAlert("File Location", file.Path, "OK");
+
+            // image.Source = ImageSource.FromStream(() =>
+            // {
+            //    var stream = file.GetStream();
+            //  return stream;
+            //});
+            await Navigation.PushAsync(new ScannerPage(file)); 
+        }
+    }
+}
---
+++ Machina/Machina/ScannerPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace Machina
+{
+    public partial class ScannerPage : ContentPage
+    {
+        public ScannerPage(MediaFile file)
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            faceImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStreamWithImageRotatedForExternalStorage();
+                return stream;
+            });
+        }
+
+        private void ContinueButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
+
